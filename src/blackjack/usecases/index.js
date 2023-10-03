ccd7a0799f16c5d7d05bd4b7a95b4290706d078c
Omar Hernandez <omarhrn@gmail.com>
2023-10-03 14:38:57 -0300

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedirCarta';
+export { valorCarta } from './valorCarta';
+export { turnoComputadora } from './turno-computadora';
+export { crearCartaHTML } from './crear-carta-html';
